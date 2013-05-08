e2ca08c7dff2b55e5c216cdccd4048c5a88d1d8f
nathanjc <nathan@nathanjc.com>
2013-05-07 21:45:32 -0600

    proxy-table shit

---
+++ proxy-table.js
@@ -0,0 +1,12 @@
+var util = require('util')
+  , http = require('http')
+  , httpProxy = require('http-proxy');
+
+httpProxy.createServer({
+    router: {
+        'localhost': 'localhost:8080',
+        'www.nathanjc.com': 'localhost:7777'
+    }
+}).listen(80);
+
+util.puts('http proxy server started on port: 80');
