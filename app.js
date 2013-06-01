c51cc85ed6ec02654eeafc2221522f47cb3107c4
nathanjc <nathan@nathanjc.com>
2013-05-31 21:44:32 -0600

    major site update

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
-var RedisStore = require('connect-redis')(express);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -17,8 +16,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 app.use(express.favicon());
-app.use(express.cookieParser());
-app.use(express.session({ secret: 'qwertyuiop', cookie: { maxAge: 60000 }}));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -33,30 +30,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// login
-app.post('/login', function (req, res) {
-  var post = req.body;
-  if (post.user == 'njchapman' && post.password == 'Mindinmind321') {
-    req.session.user_id = 777;
-    res.redirect('/admin');
-  } else {
-    res.redirect('/login');
-  }
-});
-
-function checkAuth(req, res, next) {
-  if (!req.session.user_id) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-}
-
 app.get('/', routes.index);
-app.get('/admin', checkAuth, routes.admin);
-app.get('/login', routes.login);
-app.get('/projects', routes.projects);
-app.get('/projects/thelastwebsite', routes.thelastwebsite);
 app.get('*', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Stream' });
+  res.render('index', { title: 'NathanJC' });
 };
-
-exports.login = function(req, res){
-	res.render('login', { title: 'Login' });
-};
-
-exports.admin = function(req, res){
-	res.render('admin', { title: 'Admin' });
-};
-
-exports.projects = function(req, res){
-  res.render('projects', { title: 'Projects' });
-};
-
-exports.thelastwebsite = function(req, res){
-	res.render('thelastwebsite', { title: 'The Last Website' });
-}
