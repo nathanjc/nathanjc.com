04fb71ed5f5b2d900705233e216314fc905066e3
nathanjc <nathan@nathanjc.com>
2013-07-06 16:49:55 -0600

    add minshare to proxy

--- proxy-table.js
+++ proxy-table.js
@@ -7,6 +7,8 @@ httpProxy.createServer({
         'localhost': 'localhost:8080',
         'nathanjc.com': 'localhost:3000',
         'www.nathanjc.com': 'localhost:3000',
+        'mindshare.io': 'localhost:4400',
+        'www.mindshare.io': 'localhost:4400',
         'geydemaithus.com': 'localhost:6000',
         'www.geydemaithus.com': 'localhost:6000'
     }
