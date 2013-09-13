cd32d9908be83567bafed100b0fe91e3bc512fed
nathanjc <hisbrothernathan@gmail.com>
2013-09-13 00:16:36 -0600

    add 3rdspace to proxy-table

--- proxy-table.js
+++ proxy-table.js
@@ -7,6 +7,8 @@ httpProxy.createServer({
         'localhost': 'localhost:8080',
         'nathanjc.com': 'localhost:3000',
         'www.nathanjc.com': 'localhost:3000',
+        '3rd-space.co': 'localhost:3800',
+        'www.3rd-space.co': 'localhost:3800',
         'catalysm.co': 'localhost:4400',
         'www.catalysm.co': 'localhost:4400',
         'nodex.io': 'localhost:4500',
